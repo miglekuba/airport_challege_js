3349fe178920bcf22f60b0f30c3bad3599618d89
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-11-15 19:06:02 +0000

    test not working

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -13,10 +13,15 @@ it('lands the plane at the airport', () => {
     plane.land(airport)
     expect(airport.planes).toContain(plane);
     });
-    
+
 it('the plane takes off from the airport', () => {
     plane.takeOff(airport)
-    expect(airport.planes).toNotContain(plane);
+    expect(airport.planes).not.toContain(plane);
+});
+
+it('prevents from landing when the airport is full', () => {
+    plane.land(airport)
+    expect(plane.land).toEqual("The airport is full");
 });
 }); 
 
--- src/Plane.js
+++ src/Plane.js
@@ -1,10 +1,30 @@
 'use strict';
+this.MaxCapacity = 50;
 
 function Plane () {}
+
 Plane.prototype.land = function(airport) {
-    airport.planes.push(this);
+
+if (airport.planes === this.MaxCapacity) {
+return "The airport is full";
+}
+
+else {airport.planes.push(this)};
+
 };
+
+Plane.prototype.takeOff = function(airport) {
+    airport.planes.pop(this);
+};
+
 
 function Airport () {
     this.planes = []
 };
+
+
+
+
+
+
+
